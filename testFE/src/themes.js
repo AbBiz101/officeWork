b3a626464d4fdcb0643229ece0d11286e9061b8d
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2023-09-24 14:53:49 -0500

    init

---
+++ testFE/src/themes.js
@@ -0,0 +1,12 @@
+import { createContext, useState, useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
+
+//color design
+export const tokens = (mode) => ({
+    ...(mode === 'dark' ? {}:{})
+})
+
+
+
+
+K
